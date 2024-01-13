916e131b44af8dd28fd3f854a789c795e23a63d9
Kuljeet Sran <kuljeetsran@192.168.1.2>
2024-01-13 13:35:22 +0530

    nltk QA

---
+++ app.py
@@ -0,0 +1,99 @@
+from nltk.chat.util import Chat, reflections
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+pairs=[
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",]
+    ],
+     [
+        r"what is your name ?",
+        ["My name is Chatty and I'm a chatbot ?",]
+    ],
+    [
+        r"how are you ?",
+        ["I'm doing good\nHow about You ?",]
+    ],
+    [
+        r"(.*) created (.*) ?",
+        ["Radhika created me using Python's NLTK library for Webinar 1 ",]
+    ],
+    [
+        r"sorry (.*)",
+        ["Its alright","Its OK, never mind",]
+    ],
+    [
+        r"i'm (.*) doing good",
+        ["Nice to hear that","Alright :)",]
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",]
+    ],
+    [
+        r"(.*) age?",
+        ["I'm a computer program dude\nSeriously you are asking me this?",]
+        
+    ],
+    [
+        r"what (.*) want ?",
+        ["Make me an offer I can't refuse",]
+        
+    ],
+    [
+        r"(.*) (location|city) ?",
+        ['Hyderabad, India',]
+    ],
+    [
+        r"how is weather in (.*)?",
+        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
+    ],
+    [
+        r"i work in (.*)?",
+        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
+    ],
+    [
+        r"(.*)raining in (.*)",
+        ["No rain since last week here in %2","Damn its raining too much here in %2"]
+    ],
+    [
+        r"how (.*) health(.*)",
+        ["I'm a computer program, so I'm always healthy ",]
+    ],
+    [
+        r"(.*) (sports|game) ?",
+        ["I'm a very big fan of Football",]
+    ],
+    [
+        r"who (.*) sportsperson ?",
+        ["Messy","Ronaldo","Roony"]
+    ],
+    [
+        r"who (.*) (moviestar|actor)?",
+        ["Brad Pitt"]
+    ],
+    [
+        r"quit",
+        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
+     ]
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
+    user_message = request.args.get('msg')
+    response = get_bot_response(user_message)
+    return str(response)
+
+def get_bot_response(user_message):
+    chat = Chat(pairs, reflections)
+    return chat.respond(user_message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
